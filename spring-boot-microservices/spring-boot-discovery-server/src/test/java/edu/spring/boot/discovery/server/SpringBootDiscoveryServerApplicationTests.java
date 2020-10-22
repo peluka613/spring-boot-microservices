f5d84ef0dc03701e18eb09a19153a0f8fbb8b499
Carlos Valencia <peluka613@gmail.com>
2020-10-22 11:09:18 -0500

    Create a spring-boot-discovery-server project

---
+++ spring-boot-microservices/spring-boot-discovery-server/src/test/java/edu/spring/boot/discovery/server/SpringBootDiscoveryServerApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.spring.boot.discovery.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootDiscoveryServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
