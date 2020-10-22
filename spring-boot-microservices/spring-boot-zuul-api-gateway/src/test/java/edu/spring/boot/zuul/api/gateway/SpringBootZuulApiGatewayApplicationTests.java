a6141f40490715e76c49149869846acd06958f44
Carlos Valencia <peluka613@gmail.com>
2020-10-22 14:56:24 -0500

    Create a spring-boot-zuul-api-gateway project

---
+++ spring-boot-microservices/spring-boot-zuul-api-gateway/src/test/java/edu/spring/boot/zuul/api/gateway/SpringBootZuulApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.spring.boot.zuul.api.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootZuulApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
