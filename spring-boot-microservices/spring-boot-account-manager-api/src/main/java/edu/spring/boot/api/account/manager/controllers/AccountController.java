2339c1dc3920466125a1522520b3b9855e376350
Carlos Valencia <peluka613@gmail.com>
2020-10-22 15:56:28 -0500

    Configure apis to be load balanced

--- spring-boot-microservices/spring-boot-account-manager-api/src/main/java/edu/spring/boot/api/account/manager/controllers/AccountController.java
+++ spring-boot-microservices/spring-boot-account-manager-api/src/main/java/edu/spring/boot/api/account/manager/controllers/AccountController.java
@@ -1,5 +1,8 @@
 package edu.spring.boot.api.account.manager.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,8 +11,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/account")
 public class AccountController {
 
+    @Autowired
+    private Environment env;
+
+    @Value("${spring.application.name}")
+    private String appName;
+
     @GetMapping("/status")
     public String status() {
-        return "Working ..";
+        return appName + ": " + env.getProperty("local.server.port");
     }
 }
--- spring-boot-microservices/spring-boot-users-api/src/main/java/edu/spring/boot/api/users/controllers/UserController.java
+++ spring-boot-microservices/spring-boot-users-api/src/main/java/edu/spring/boot/api/users/controllers/UserController.java
@@ -1,5 +1,8 @@
 package edu.spring.boot.api.users.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,8 +11,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/users")
 public class UserController {
 
+    @Autowired
+    private Environment env;
+
+    @Value("${spring.application.name}")
+    private String appName;
+
     @GetMapping("/status")
     public String status() {
-        return "Working";
+        return appName + ": " + env.getProperty("local.server.port");
     }
 }
