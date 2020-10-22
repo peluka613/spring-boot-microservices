1453fdf4fbe025ff28f734fca3a7c8f6680301a4
Carlos Valencia <peluka613@gmail.com>
2020-10-22 11:35:36 -0500

    Create a spring-boot-users-api project

---
+++ spring-boot-microservices/spring-boot-users-api/src/main/java/edu/spring/boot/api/users/controllers/UserController.java
@@ -0,0 +1,15 @@
+package edu.spring.boot.api.users.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+
+    @GetMapping
+    public String status() {
+        return "Working";
+    }
+}
---
+++ spring-boot-microservices/spring-boot-users-api/src/test/java/edu/spring/boot/api/users/SpringBootUsersApiApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.spring.boot.api.users;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootUsersApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
